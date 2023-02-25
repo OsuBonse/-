ed1761cf9a592a07a6990504c53f456adb64fee8
Bonse <84657555+OsuBonse@users.noreply.github.com>
2023-02-26 02:12:02 +0300

    Update ltx1.py

--- ltx1.py
+++ ltx1.py
@@ -2,16 +2,26 @@ import pandas as pd
 
 df = pd.read_csv('ltx.csv')
 df_group = df.groupby(['PurposeOfTheAssignment', 'Koatyy','Price','ValueNGO'], as_index=False)
-a = df['PurposeOfTheAssignment'].value_counts()
-b = df.groupby('PurposeOfTheAssignment')['Koatyy'].sum().sort_values(ascending=False)
-c = df.groupby('PurposeOfTheAssignment')['Price'].median().sort_values(ascending=False)
-d = df.groupby('PurposeOfTheAssignment')['ValueNGO'].median().sort_values(ascending=False)
+a = df['PurposeOfTheAssignment'].value_counts().reset_index()
+b = df.groupby('PurposeOfTheAssignment')['Koatyy'].sum().sort_values(ascending=False).reset_index()
+c = df.groupby('PurposeOfTheAssignment')['Price'].median().sort_values(ascending=False).reset_index()
+d = df.groupby('PurposeOfTheAssignment')['ValueNGO'].median().sort_values(ascending=False).reset_index()
+
+a1 = a['index']
+a1_1 = a['PurposeOfTheAssignment']
+b1 = b['Koatyy']
+c1 = c['Price']
+d1 = d['ValueNGO']
+
 print('--------------------------')
-print(a)
+print(a1)
 print('--------------------------')
-print(b)
+print(a1_1)
 print('--------------------------')
-print(c)
+print(b1)
 print('--------------------------')
-print(d)
+print(c1)
 print('--------------------------')
+print(d1)
+print('--------------------------')
+
