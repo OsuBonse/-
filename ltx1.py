685c339a29c225aa0b5fc1ae0905de7b6dac28e9
Bonse <84657555+OsuBonse@users.noreply.github.com>
2023-02-26 03:24:53 +0300

    Update ltx1.py

--- ltx1.py
+++ ltx1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from openpyxl import load_workbook
 
 df = pd.read_csv('ltx.csv')
 df_group = df.groupby(['PurposeOfTheAssignment', 'Koatyy','Price','ValueNGO'], as_index=False)
@@ -12,6 +13,7 @@ a1_1 = a['PurposeOfTheAssignment']
 b1 = b['Koatyy']
 c1 = c['Price']
 d1 = d['ValueNGO']
+length = a.shape[0]
 
 print('--------------------------')
 print(a1)
@@ -25,3 +27,18 @@ print('--------------------------')
 print(d1)
 print('--------------------------')
 
+data = {'Цільове призначення': a1 , 'Кількість угод': a1_1, 'Площа, га': b1, 'Ціна (вартість), грн./га': c1, 'Значення НГО, грн./га': d1}
+df = pd.DataFrame(data)
+
+workbook = load_workbook(filename='opus.xlsx')
+worksheet = workbook['123']
+
+last_row = worksheet.max_row
+while worksheet.cell(row=last_row, column=3).value is None and last_row > 1:
+    last_row -= 1
+
+print(last_row)
+
+with pd.ExcelWriter('opus.xlsx') as writer:
+    pd.DataFrame([length]).to_excel(writer, sheet_name='123',startrow=last_row)
+    df.to_excel(writer, sheet_name='123', index=True,startrow=8,startcol=1)
